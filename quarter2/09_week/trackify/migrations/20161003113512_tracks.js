c224385b7524fd2ac297c5ef17fbe32a8d45e1ec
rrathbone <rachellerathbone@gmail.com>
2016-10-03 14:32:06 -0700

    initial commit

---
+++ quarter2/09_week/trackify/migrations/20161003113512_tracks.js
@@ -0,0 +1,15 @@
+'use strict';
+
+exports.up = function(knex) {
+  return knex.schema.createTable('tracks', (table) => {
+    table.increments();
+    table.string('title').notNullable().defaultTo('');
+    table.string('artist').notNullable().defaultTo('');
+    table.integer('likes').notNullable().defaultTo(0);
+    table.timestamps(true, true);
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('tracks');
+};
---
+++ quarter2/09_week/trackify/seeds/1_tracks.js
@@ -0,0 +1,41 @@
+'use strict';
+
+exports.seed = function(knex) {
+  return knex('tracks').del()
+    .then(() => {
+      return knex('tracks').insert([{
+        id: 1,
+        title: 'Here Comes the Sun',
+        artist: 'The Beatles',
+        likes: 28808736,
+        created_at: new Date('2016-06-26 14:26:16 UTC'),
+        updated_at: new Date('2016-06-26 14:26:16 UTC')
+      }, {
+        id: 2,
+        title: 'Hey Jude',
+        artist: 'The Beatles',
+        likes: 20355655,
+        created_at: new Date('2016-06-26 14:26:16 UTC'),
+        updated_at: new Date('2016-06-26 14:26:16 UTC')
+      }, {
+        id: 3,
+        title: 'Send My Love',
+        artist: 'Adele',
+        likes: 39658471,
+        created_at: new Date('2016-06-26 14:26:16 UTC'),
+        updated_at: new Date('2016-06-26 14:26:16 UTC')
+      }, {
+        id: 4,
+        title: 'Hello',
+        artist: 'Adele',
+        likes: 538300301,
+        created_at: new Date('2016-06-26 14:26:16 UTC'),
+        updated_at: new Date('2016-06-26 14:26:16 UTC')
+      }]);
+    })
+    .then(() => {
+      return knex.raw(
+        "SELECT setval('tracks_id_seq', (SELECT MAX(id) FROM tracks));"
+      );
+    });
+};
