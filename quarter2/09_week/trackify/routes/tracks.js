d81abf5387d2257c6c0ce75d60f17dda522045eb
rrathbone <rachellerathbone@gmail.com>
2016-10-04 11:14:09 -0700

    Add GET /tracks/:id middleware

--- quarter2/09_week/trackify/routes/tracks.js
+++ quarter2/09_week/trackify/routes/tracks.js
@@ -1,7 +1,41 @@
 'use strict';
 
+const boom = require('boom');
 const express = require('express');
+const knex = require('../knex');
+const { camelizeKeys } = require('humps');
 
 const router = express.Router();
+
+router.get('/tracks', (_req, res, next) => {
+  knex('tracks')
+    .orderBy('title')
+    .then((rows) => {
+      const tracks = camelizeKeys(rows);
+
+      res.send(tracks);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.get('/tracks/:id', (req, res, next) => {
+    knex('tracks')
+    .where('id', req.params.id)
+    .first()
+    .then((row) => {
+      if (!row) {
+        throw boom.create(404, 'Not Found');
+      }
+
+      const track = camelizeKeys(row);
+
+      res.send(track);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
